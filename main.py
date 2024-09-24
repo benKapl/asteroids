463bffaadb3194adf718ca5deb69abbb226b7ae6
benKapl <benji1.kaplan@gmail.com>
2024-09-24 14:00:48 +0200

    init game loop

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 import pygame
 
+from constants import *
+
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    while True:
+        # Allow user to close the window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+        screen.fill(color="black")
+        screen = pygame.display.flip()
+
+    
+    # print("Starting asteroids!")
+    # print("Screen width:", SCREEN_WIDTH)
+    # print("Screen height:", SCREEN_HEIGHT)
 
 if __name__ == "__main__":
     main()

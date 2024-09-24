32e0fd4a49a8b890cd6219d248c94179c5a453e9
benKapl <benji1.kaplan@gmail.com>
2024-09-24 14:19:32 +0200

    addeded FPS management and corrected screen display

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ from constants import *
 
 def main():
     pygame.init()
+    # creates screen
+    screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(color="black")
+
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         # Allow user to close the window
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
-        screen.fill(color="black")
-        screen = pygame.display.flip()
-
+        pygame.display.flip()
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
     
     # print("Starting asteroids!")
     # print("Screen width:", SCREEN_WIDTH)
